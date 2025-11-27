df252c482d9919ba3f26d2c242142f1d4ecb826f
rezbites <shashank.30choudhary@gmail.com>
2025-11-27 12:34:24 +0530

    post

--- main/routes.py
+++ main/routes.py
@@ -4,31 +4,15 @@ from PIL import Image
 from flask import render_template, flash, redirect, url_for, request
 from main import app, db, bcrypt
 from main.models import User, Post
-from main.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from main.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 
 
-
-posts = [ 
-{
-    'author': 'John Doe',
-    'title': 'Blog Post 1',
-    'content': 'This is the content of the first blog post.',
-    'date_posted': 'April 20, 2023'
-},
-
-{
-    'author': 'Jane Smith',
-    'title': 'Blog Post 2',
-    'content': 'This is the content of the second blog post.',
-    'date_posted': 'April 21, 2023'
-}
-
-]
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 @app.route('/about')
@@ -102,8 +86,6 @@ def account():
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
 
-
-
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
@@ -114,3 +96,15 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',image_file=image_file, form=form) 
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title= form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template('create_post.html', title='New Posts', form=form)
