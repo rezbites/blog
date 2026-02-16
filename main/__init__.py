c7c0c8e870d3b1267ae65fe49aa8ecd8afea1ffb
rezbites <shashank.30choudhary@gmail.com>
2026-02-16 17:51:42 +0530

    trial db sample table

--- main/__init__.py
+++ main/__init__.py
@@ -3,18 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'df20cd524044f455dc80007d4bb3f328'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-# ✅ Create tables automatically when app starts
+# ✅ IMPORT MODELS BEFORE create_all
+from main.models import User, Post
+
+# ✅ Now create tables
 with app.app_context():
     db.create_all()
 
